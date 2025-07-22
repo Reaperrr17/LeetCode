3e6564a3b159274e80260d03d8cfc6c3b04a61f4
Debojyoti <debojyoti.iiitbh@gmail.com>
2025-07-22 11:05:53 +0530

    Time: 0 ms (100%), Space: 20.7 MB (26.17%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -3,9 +3,8 @@ public:
     int singleNumber(vector<int>& nums) {
 
         int ans = nums[0];
-        int N = nums.size();
 
-        for(int i=1;i<N;i++){
+        for(int i=1;i<nums.size();i++){
             ans = ans^nums[i];
         }
 
