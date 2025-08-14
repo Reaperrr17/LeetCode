523620d35477bcd681ef822e57611ce4d4562cd4
Debojyoti <debojyoti.iiitbh@gmail.com>
2025-08-14 18:14:18 +0530

    Time: 0 ms (100%), Space: 14 MB (79.49%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,42 +1,47 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-
+        
         int N = nums.size();
+
         if(nums[0]<=nums[N-1]){
             return nums[0];
         }
-        else if(nums[N-1]<nums[N-2]){
-            return nums[N-1];
-        }
         else{
-            int st=1;
-            int end = N-2;
-            int ans=INT_MAX;
-
-            while(st<=end){
-
-                int mid = st + (end-st)/2;
-
-                if(nums[mid]<=nums[mid-1] && nums[mid]<=nums[mid+1]){    
-                    ans =  nums[mid];
-                    break;
-                }
-                else if(nums[mid]>=nums[st]){
-                    if(nums[st]<ans){
-                        ans = nums[st];
-                    }
-                    st = mid+1;
-                }
-                else if(nums[mid]<=nums[end]){
-                    if(nums[mid]<ans){
-                        ans = nums[mid];
-                    }
-                    end = mid-1;
-                }
+            if(nums[N-1]<nums[N-2]){
+                return nums[N-1];
             }
-            return ans;
+            else{
+                int st=1;
+                int end=N-2;
+                int ans;
+
+                while(st<=end){
+                    int mid = st + (end-st)/2;
+
+                    if(nums[mid]<nums[mid-1] && nums[mid]<nums[mid+1]){
+                        ans = nums[mid];
+                        break;
+                    }
+                    else if(nums[mid]>=nums[st]){
+                        if(nums[end]>=nums[st]){
+                            end = mid-1;
+                        }
+                        else{
+                            st = mid+1;
+                        }
+                    }
+                    else{
+                        if(nums[end]<=nums[mid]){
+                            st = mid+1;
+                        }
+                        else{
+                            end = mid-1;
+                        }
+                    }
+                }
+                return ans;
+            }
         }
-        
     }
 };
