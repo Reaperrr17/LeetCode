4340fa69be47f142376f48322482f705a07d947a
Debojyoti <debojyoti.iiitbh@gmail.com>
2025-08-21 21:58:58 +0530

    Time: 7 ms (72.21%), Space: 22.8 MB (73.46%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
-    bool ispossible(int maxbananas,vector <int> & piles,int maxhours){
-        
-        int hours = 0;
+    bool ispossible(long long maxbananas,vector <int> &piles,int h){
+        long long time = 0;
         for(int i=0;i<piles.size();i++){
             if(piles[i]<=maxbananas){
-                hours++;
+                time++;
             }else{
-                hours += ceil(piles[i]/(double)maxbananas);
+                time += (piles[i] + maxbananas - 1) / maxbananas;
             }
-            if(hours>maxhours){
+            if(time>h){
                 return false;
             }
         }
         return true;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        
-            int st = 1;
-            long long end = *(max_element(piles.begin(),piles.end()));
-            int ans;
-            
-            while(st<=end){
-                long long mid = st + (end-st)/2;
-                
-                if(ispossible(mid,piles,h)){
-                    ans = mid;
-                    end = mid-1;
-                }else{
-                    st = mid+1;
-                }
+
+        long long st = 1;
+        long long end = *(max_element(piles.begin(),piles.end()));
+        long long ans;
+
+        while(st<=end){
+
+            long long mid = st + (end-st)/2;
+
+            if(ispossible(mid,piles,h)){
+                ans = mid;
+                end = mid-1;
+            }else{
+                st = mid+1;
             }
-            return ans;
         }
+        return (int)ans;
+    }
 };
