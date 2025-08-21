b1ad0415071eb43933a2d88b4852e99a28fd7284
Debojyoti <debojyoti.iiitbh@gmail.com>
2025-08-21 19:13:47 +0530

    Time: 0 ms (100%), Space: 26 MB (91.83%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -3,55 +3,44 @@ public:
     int singleNonDuplicate(vector<int>& nums) {
         
         int N = nums.size();
-
         if(N==1){
             return nums[0];
-        }
-        else{
+        }else{
             if(nums[0]!=nums[1]){
                 return nums[0];
-            }
-            else if(nums[N-1]!=nums[N-2]){
+            }else if(nums[N-1]!=nums[N-2]){
                 return nums[N-1];
-            }
-            else{
-
+            }else{
                 int st = 1;
                 int end = N-2;
-                int ans;
 
                 while(st<=end){
+
                     int mid = st + (end-st)/2;
 
-                    if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
-                        ans = nums[mid];
-                        break;
-                    }
-                    else if(nums[mid]==nums[mid-1]){
-                        int num1 = mid-1;
-                        int num2 = N-1-mid;
-                        if(num1%2==0){
-                            st = mid+1;
-                        }
-                        else{
-                            end = mid-2;
-                        }
-                    }
-                    else{
+                    if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]){
+                        return nums[mid];
+                    }else if(nums[mid]==nums[mid+1]){
                         int num1 = mid;
-                        int num2 = N-1-mid-1;
-                        if(num1%2==0){
+                        int num2 = N-mid-2;
+                        if(num1%2!=0){
+                            end = mid-1;
+                        }else{
                             st = mid+2;
                         }
-                        else{
-                            end = mid-1;
+                    }else{
+                        int num1 = mid-1;
+                        int num2 = N-mid-1;
+                        if(num1%2!=0){
+                            end = mid-2;
+                        }else{
+                            st = mid+1;
                         }
                     }
                 }
+                return  0;
 
-                return ans;
             }
-
         }
     }
 };
