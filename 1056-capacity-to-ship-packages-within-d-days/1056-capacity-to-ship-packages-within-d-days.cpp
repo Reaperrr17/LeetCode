f5a421769f50072681f62cb0c054b4044d9958ac
Debojyoti <debojyoti.iiitbh@gmail.com>
2025-08-21 21:23:29 +0530

    Time: 9 ms (78.56%), Space: 35 MB (73.67%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -1,40 +1,35 @@
 class Solution {
-  public:
-    bool ispossible(int maxpages,vector <int> &books,int k){
+public:
+    bool ispossible(int maxweight,vector <int> &weights,int days){
+        int pseudodays = 1;
         long long sum = 0;
-        int stu = 1;
-        for(int i=0;i<books.size();i++){
-            if(books[i]>maxpages){
-                return false;
-            }
-            sum += books[i];
-            if(sum>maxpages){
-                stu++;
-                sum = 0;
-                sum = books[i];
-            }
-            if(stu>k){
-                return false;
+        for(int i=0;i<weights.size();i++){
+            sum += weights[i];
+            if(sum>maxweight){
+                pseudodays++;
+                if(pseudodays>days){
+                    return false;
+                }
+                sum = weights[i];
             }
         }
         return true;
     }
-    int shipWithinDays(vector<int>& books, int k) {
-        // code here
-            int st = *(max_element(books.begin(),books.end()));
-            long long end = accumulate(books.begin(),books.end(),0);
-            int ans;
-            
-            while(st<=end){
-                long long mid = st + (end-st)/2;
-                
-                if(ispossible(mid,books,k)){
-                    ans = mid;
-                    end = mid-1;
-                }else{
-                    st = mid+1;
-                }
+    int shipWithinDays(vector<int>& weights, int days) {
+        int ans;
+        int st = *(max_element(weights.begin(),weights.end()));
+        int end = accumulate(weights.begin(),weights.end(),0);
+
+        while(st<=end){
+            int mid = st + (end-st)/2;
+
+            if(ispossible(mid,weights,days)){
+                ans = mid;
+                end = mid-1;
+            }else{
+                st = mid+1;
             }
-            return ans;
+        }
+        return ans;
     }
 };
