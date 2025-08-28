486a917a46d46407ad8e651e0186d0981740b7a0
Debojyoti <debojyoti.iiitbh@gmail.com>
2025-08-28 17:17:05 +0530

    Time: 0 ms (100%), Space: 27.3 MB (20.14%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
+        
         int N = height.size();
+        int totalwater = 0;
 
         vector <int> suffix(N);
         vector <int> prefix(N);
 
         prefix[0] = height[0];
         suffix[N-1] = height[N-1];
-        int sum = 0;
 
+        int maxheight1 = height[0];
+        int maxheight2 = height[N-1];
 
         for(int i=1;i<N;i++){
-            if(height[i]>prefix[i-1]){
-                prefix[i]=height[i];
-            }else{
-                prefix[i]=prefix[i-1];
+            prefix[i] = maxheight1;
+            if(height[i]>maxheight1){
+                maxheight1 = height[i];
             }
         }
 
         for(int i=N-2;i>=0;i--){
-            if(height[i]>suffix[i+1]){
-                suffix[i] = height[i];
-            }else{
-                suffix[i] = suffix[i+1];
+            suffix[i] = maxheight2;
+            if(height[i]>maxheight2){
+                maxheight2 = height[i];
             }
         }
 
         for(int i=1;i<N-1;i++){
-            if(height[i]<prefix[i-1] && height[i]<suffix[i+1]){
-                sum += min(suffix[i+1],prefix[i-1])-height[i];
+            if(height[i]<suffix[i] && height[i]<prefix[i]){
+                totalwater += min(suffix[i],prefix[i]) - height[i];
             }
         }
 
-        return sum;
+        return totalwater;
     }
 };
