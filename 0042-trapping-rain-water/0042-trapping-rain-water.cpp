fdbcb134aabaec1369978a7840078a2b5a5bb2f3
Debojyoti <debojyoti.iiitbh@gmail.com>
2025-08-28 18:09:10 +0530

    Time: 0 ms (100%), Space: 26 MB (60.62%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -3,31 +3,32 @@ public:
     int trap(vector<int>& height) {
         
         int N = height.size();
+        int maxheight = height[0];
+        int maxheightindex = 0;
+        int max1 = height[0];
+        int max2 = height[N-1];
         int totalwater = 0;
 
-        vector <int> suffix(N);
-        vector <int> prefix(N);
-
-        int maxheight1 = height[0];
-        int maxheight2 = height[N-1];
-
         for(int i=1;i<N;i++){
-            prefix[i] = maxheight1;
-            if(height[i]>maxheight1){
-                maxheight1 = height[i];
+            if(height[i]>maxheight){
+                maxheight = height[i];
+                maxheightindex = i;
             }
         }
 
-        for(int i=N-2;i>=0;i--){
-            suffix[i] = maxheight2;
-            if(height[i]>maxheight2){
-                maxheight2 = height[i];
+        for(int i=1;i<maxheightindex;i++){
+            if(height[i]<max1){
+                totalwater += max1 - height[i];
+            }else{
+                max1 = height[i];
             }
         }
 
-        for(int i=1;i<N-1;i++){
-            if(height[i]<suffix[i] && height[i]<prefix[i]){
-                totalwater += min(suffix[i],prefix[i]) - height[i];
+        for(int i=N-2;i>maxheightindex;i--){
+            if(height[i]<max2){
+                totalwater += max2 - height[i];
+            }else{
+                max2 = height[i];
             }
         }
 
