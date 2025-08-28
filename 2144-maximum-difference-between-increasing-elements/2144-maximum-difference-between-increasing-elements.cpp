025a26f2953a60baf7cc2636cac838dac71b49d4
Debojyoti <debojyoti.iiitbh@gmail.com>
2025-08-28 11:51:17 +0530

    Time: 0 ms (100%), Space: 11.9 MB (66.9%) - LeetHub

--- 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.cpp
+++ 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.cpp
@@ -3,26 +3,19 @@ public:
     int maximumDifference(vector<int>& nums) {
         
         int N = nums.size();
+        int min = nums[0];
         int maxdiff = INT_MIN;
-        vector <int> suffix(N);
 
-        suffix[N-1] = nums[N-1];
-        int maxelement = nums[N-1];
-
-        for(int i=N-2;i>=0;i--){
-            if(nums[i]>maxelement){
-                suffix[i] = suffix[i+1];
-                maxelement = nums[i];
+        for(int i=1;i<N;i++){
+            if(nums[i]<min){
+                min = nums[i];
             }else{
-                suffix[i] = maxelement;
+                if(nums[i]-min>maxdiff){
+                    maxdiff = nums[i]-min;
+                }
             }
         }
-
-        for(int i=0;i<N-1;i++){
-            if(suffix[i]-nums[i]>maxdiff){
-                maxdiff = suffix[i]-nums[i];
-            }
-        }
+        
         if(maxdiff<=0){
             return -1;
         }else{
