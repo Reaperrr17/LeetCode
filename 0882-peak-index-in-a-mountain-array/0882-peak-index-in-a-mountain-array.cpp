3a31cbb4e54c3979aaaaa525ca3b0e884706f03b
Debojyoti <debojyoti.iiitbh@gmail.com>
2025-09-01 12:16:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    int peakIndexInMountainArray(vector<int>& nums) {
+    int peakIndexInMountainArray(vector<int>& arr) {
+
         int N = nums.size();
-        
-        int st=1;
-        int end=N-2;
-        int ans;
+        int st = 1;
+        int end = N-2;
 
         while(st<=end){
 
             int mid = st + (end-st)/2;
 
-            if(nums[mid]>=nums[mid-1] && nums[mid]>=nums[mid+1]){
-                ans = mid;
-                break;
-            }
-            else if(nums[mid]<=nums[mid+1]){
-                st = mid+1;
-            }
-            else{
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+                return mid;
+            }else if(nums[mid]>nums[mid+1]){
                 end = mid-1;
+            }else{
+                st = mid+1;
             }
         }
 
-        return ans;
+        return 0;
+        
     }
 };
