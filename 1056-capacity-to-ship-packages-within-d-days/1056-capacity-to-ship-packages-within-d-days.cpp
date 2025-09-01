b9e289285537c1b0368700ac9785e8cbdd249e0a
Debojyoti <debojyoti.iiitbh@gmail.com>
2025-09-01 13:15:34 +0530

    Time: 17 ms (17.43%), Space: 34.9 MB (77.49%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
-    bool ispossible(int maxweight,vector <int> &weights,int days){
-        int pseudodays = 1;
+    bool ispossible(long long maxweight,vector<int>& weights, int days){
+
+        int numofdays = 1;
         long long sum = 0;
+
         for(int i=0;i<weights.size();i++){
             sum += weights[i];
             if(sum>maxweight){
-                pseudodays++;
-                if(pseudodays>days){
+                numofdays++;
+                if(numofdays>days){
                     return false;
                 }
                 sum = weights[i];
@@ -16,12 +18,14 @@ public:
         return true;
     }
     int shipWithinDays(vector<int>& weights, int days) {
+        
+        long long st = *(max_element(weights.begin(),weights.end()));
+        long long end = accumulate(weights.begin(),weights.end(),0);
         int ans;
-        int st = *(max_element(weights.begin(),weights.end()));
-        int end = accumulate(weights.begin(),weights.end(),0);
 
         while(st<=end){
-            int mid = st + (end-st)/2;
+
+            long long mid = st + (end-st)/2;
 
             if(ispossible(mid,weights,days)){
                 ans = mid;
@@ -30,6 +34,7 @@ public:
                 st = mid+1;
             }
         }
+
         return ans;
     }
 };
